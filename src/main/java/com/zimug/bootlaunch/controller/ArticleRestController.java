1e102a57a4015a45eece609e53c48f4e649f05da
qiaokun <qkbeyond@gmail.com>
2020-03-27 11:52:08 +0800

    swagger 手动注释文档后 每个接口都要重新写 否则不能自动生成所有的api
    //
    //    @ApiOperation(value="添加文章", notes="添加新的文章",tags = "Article", httpMethod = "POST")
    //    @ApiResponses({

--- src/main/java/com/zimug/bootlaunch/controller/ArticleRestController.java
+++ src/main/java/com/zimug/bootlaunch/controller/ArticleRestController.java
@@ -2,6 +2,9 @@ package com.zimug.bootlaunch.controller;
 
 import com.zimug.bootlaunch.model.AjaxResponse;
 import com.zimug.bootlaunch.model.Article;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,12 @@ import java.util.Date;
 
 @RequestMapping("/rest")
 public class ArticleRestController {
+//
+//    @ApiOperation(value="添加文章", notes="添加新的文章",tags = "Article", httpMethod = "POST")
+//    @ApiResponses({
+//            @ApiResponse(code=200,message="成功",response = AjaxResponse.class),
+//            @ApiResponse(code=400,message="用户输入错误",response = AjaxResponse.class)
+//    })
 
     // RequestMapping() , 可以简化成  @PostMapping, @GetMapping @PutMapping @DeleteMapping等方法
 
